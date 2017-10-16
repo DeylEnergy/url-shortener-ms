dad395beea157717dfd989465d33c9a9ef9390ef
Deyl <soulvisman@gmail.com>
2017-10-16 22:42:27 +0600

    it can generate an url

--- server.js
+++ server.js
@@ -2,22 +2,42 @@ const mongo = require('mongojs');
 const db = mongo('urlsdb', ['urls_collection']);
 const express = require('express');
 const app = express();
+const serviceUrl = 'https://myshortapp.com/';
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
-app.get('/new/:url', (req, res) => {
-  let getNextSequenceValue = sequenceName => {
-    const sequenceDocument = db.counters.findAndModify({
-      query: {_id: sequenceName},
-      update: {$inc: {sequence_value: 1}},
-      new: true
+app.get('/new/:original_url', (req, res) => {
+  let incrementNewUrl = collectionName => {
+    return new Promise((resolve, reject) => {
+      returnedId = db.counters.findAndModify({
+        query: {_id: collectionName},
+        update: {$inc: {sequence_value: 1}},
+        new: true
+      }, (err, data) => {
+        if (err) throw err;
+        resolve(data.sequence_value);
+      });
     });
-    return sequenceDocument.sequence_value;
   };
-  let data = {};
-  db.urls_collection.find((err, data) => {
-    res.json(data);
+  incrementNewUrl('url_id').then((newId) => {
+    db.urls_collection.insert({
+      _id: newId,
+      original_url: req.params.original_url
+    });
+    new Promise((resolve, reject) => {
+      db.urls_collection.find({_id: newId}).toArray((err, data) => {
+        if (err) throw err;
+        resolve(data[0]);
+      });
+    }).then((urlObj) => {
+      res.json({
+        original_url: urlObj.original_url,
+        short_url: `${serviceUrl}${urlObj._id}`
+      });
+    });
+  }).catch((err) => {
+    console.log(err);
   });
 });
 
